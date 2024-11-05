1104093e3712de7d3415c7ec8977f8b97613d278
Fabro324 <jorge.fabro@iesdoctorbalmis.com>
2024-11-05 11:27:28 +0100

    Cambio a detalle_producto.php

--- public/detalle_producto.php
+++ public/detalle_producto.php
@@ -14,19 +14,23 @@ try {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detalle del Producto</title>
 </head>
+
 <body>
-<?php
-// Aquí puedes usar las variables $nombre, $precio y $imagen para mostrar los detalles del producto
-echo "<div style='background-color:white; width:300px; height:auto; text-align:center;border-radius:30px; border: 2px solid black;'>
-        <img src='$imagen' alt='Imagen del producto' style='width:200px; height:150px; margin-top:20px;'>
-        <h3 style='color:black;'>$nombre</h3>
-        <h3 style='color:black;'> Precio: $precio €</h3>
-      </div>";
-?>
+
+    <h1 style="display:flex; justify-content:center;">Detalle del Producto</h1>
+
+    <div style='background-color:white; width:300px; height:auto; text-align:center;border-radius:30px; border: 2px solid black;'>
+        <img src='<?php echo $imagen; ?>' alt='Imagen del producto' style='width:200px; height:150px; margin-top:20px;'>
+        <h3 style='color:black;'><?php echo $nombre; ?></h3>
+        <h3 style='color:black;'> Precio: <?php echo $precio; ?> €</h3>
+    </div>
+
 </body>
+
 </html>
