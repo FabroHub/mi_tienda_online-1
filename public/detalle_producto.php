f96757f316f6872b751ac870d3e6777f80f4d709
Fabro324 <jorge.fabro@iesdoctorbalmis.com>
2024-11-05 11:50:27 +0100

    He metido el fondo y volver a la tienda

--- public/detalle_producto.php
+++ public/detalle_producto.php
@@ -19,9 +19,17 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detalle del Producto</title>
+    <link rel="icon" type="image/x-icon" href="https://cdn.pixabay.com/photo/2020/04/20/21/18/tree-5069963_960_720.jpg">
+    <link rel="stylesheet" href="../assets/css/styles.css">
+    <link rel="icon" type="image/x-icon" href="https://cdn.pixabay.com/photo/2020/04/20/21/18/tree-5069963_960_720.jpg">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/9e805df0a7.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
+<a style="text-decoration:none; color:white; width:auto;" href='../public/public.php'>
+        <h2> <i class="fa-solid fa-left-long" style="color: #ffffff;"></i> Volver a la tienda</h2>
+    </a>
 
     <h1 style="display:flex; justify-content:center;">Detalle del Producto</h1>
 
